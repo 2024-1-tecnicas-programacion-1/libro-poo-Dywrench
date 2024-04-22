2d9282821a93fd31e64a53a168510060227bdecd
gabo.llanos776@gmail.com <gabo.llanos776@gmail.com>
2024-04-22 09:00:54 -0500

    primera versión código- Java- Juan Pablo velasquez hernandez

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,5 +1,37 @@
 package com.mycompany.libropoo;
+import java.time.Year;
+import java.util.Scanner;
+import java.time.LocalDate;
 
 public class Libro {
     
+    private String titulo;
+    private String autor;
+    private String genero;
+    boolean leido;
+
+    private int anoPublicacion;
+    private int anoAtual = Year.now().getValue(); 
+   public Libro (String titulo, String autor,String genero){
+   this.titulo=titulo;
+   this.autor=autor;
+   this.genero=genero;
+   this.leido=false;
+
+   }
+   
+   public  String mostrarInformacion(){
+   return("Título:"+titulo+","+"Autor:"+autor+","+"Año de publicación:"+anoPublicacion+"Género:"+genero);
+}
+   public void marcarleido(){
+       leido =true;
+   }
+   
+   public boolean esantiguo (){
+   int validarAntiguedad= anoPublicacion-anoAtual;
+   return (validarAntiguedad>=50 );
+   
+   }
+ 
+  
 }
--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,40 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main (String [ ] args) {
+    Scanner sc=new Scanner(System.in);
+    
+    System.out.println("Ingrese el titulo del libro");
+    String titulo=sc.next();
+    
+    System.out.println("Ingrese el autor del libro");
+    String autor=sc.next();
+    
+    System.out.println("Ingrese el genero del libro");
+    String genero=sc.next();
+    
+    
+    int anoPublicacion=sc.nextInt();
+    Libro biblioteca = new Libro (  titulo,  autor,  genero);
+    
+    String informacion=biblioteca.mostrarInformacion();
+    boolean yaSeleyo=biblioteca.leido;
+    boolean antiguedad=biblioteca.esantiguo();
+        System.out.println(informacion);
+    if (yaSeleyo=true){
+        System.out.println("ya fue leido");
+    }
+    else {System.out.println("no ha sido leido");
+    }
+       if(antiguedad=true){
+           System.out.println("El libro es antiguo");
+       }
+       else {
+           System.out.println("El libro no es aniguo");
+           System.out.println("El año de publicacion es:"+anoPublicacion);
+       }
+    }
     
 }
