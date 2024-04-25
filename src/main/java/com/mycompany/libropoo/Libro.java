c537e946ed6b114552e4150d03756dc16e37a2ec
gabo.llanos776@gmail.com <gabo.llanos776@gmail.com>
2024-04-25 08:46:58 -0500

    complejidad temporal y eliminacion de metodo año actual

--- src/main/java/com/mycompany/libropoo/Libro.java
+++ src/main/java/com/mycompany/libropoo/Libro.java
@@ -1,44 +1,48 @@
 package com.mycompany.libropoo;
+
 import java.time.Year;
 import java.util.Scanner;
 import java.time.LocalDate;
 
 public class Libro {
-    
+
     private String titulo;
     private String autor;
     private String genero;
-    boolean leido;
+    private boolean leido;
     private int anoPublicacion;
-    private int anoAtual = Year.now().getValue(); 
-    
-   public Libro (String titulo, String autor,String genero,int anoPublicacion ){
-   this.titulo=titulo;
-   this.autor=autor;
-   this.anoPublicacion=anoPublicacion;
-   this.genero=genero;
-   this.leido=false;
 
-   }
-   
-   public  String mostrarInformacion(){
-   return("Título: "+titulo+", "+"Autor: "+autor+", "+"Año de publicación: "+anoPublicacion+", "+" Género: "+genero);
-}
-   public void marcarleido(){
-       leido =true;
-   }
-   
-    boolean getLeido (){
-    return leido;
+    // complejidad temporal O(1)
+    public Libro(String titulo, String autor, String genero, int anoPublicacion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacion = anoPublicacion;
+        this.genero = genero;
+        this.leido = false;
+
     }
-    
-   public boolean esantiguo (){
-   int validarAntiguedad= anoAtual-anoPublicacion;
-   return (validarAntiguedad>50 );
-   
-   }
-    public int anoact(){
-    return anoAtual;
-   }
-  
+
+    // complejidad temporal O(1)
+    public String mostrarInformacion() {
+        return ("Título: " + titulo + ", " + "Autor: " + autor + ", " + "Año de publicación: " + anoPublicacion + ", " + " Género: " + genero);
+    }
+
+    // complejidad temporal O(1)
+    public void marcarleido() {
+        leido = true;
+    }
+
+    // complejidad temporal O(1)
+    boolean getLeido() {
+        return leido;
+    }
+
+    // complejidad temporal O(1)
+    public boolean esantiguo() {
+        int anoAtual = Year.now().getValue();
+        int validarAntiguedad = anoAtual - anoPublicacion;
+        return (validarAntiguedad > 50);
+
+    }
+
 }
